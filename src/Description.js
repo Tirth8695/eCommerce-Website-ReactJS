f7ffbf0301c57e74e778c23184380573064c80e8
Shashank Mistry <shashankmistry30@gmail.com>
2023-02-04 19:08:30 -0500

    add comments JSX

--- src/Description.js
+++ src/Description.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 import './Home.css';
 import { useLocation } from "react-router-dom";
@@ -6,37 +6,57 @@ import Navigation from './Navigation';
 
 function Description(props) {
     const location = useLocation();
-    console.log("Debug",location.state.item.description);
+    console.log("Debug", location.state.item.description);
 
-    const rating = [  {    
-        commentId: 1,    
-        userId: 101,    
-        username: "jane_doe",    
-        productId: 1,    
-        rating: 4,    
-        comment: "I love this shirt! It fits well and the material is so soft. I would definitely recommend it.",    
-        image: location.state.item.image  },  
-        {    
-        commentId: 2,    
-        userId: 102,    
-        username: "john_doe",    
-        productId: 2,    
-        rating: 3,    
-        comment: "The shirt is okay, but the sizing is a bit off. I would suggest ordering a size up.",    
-        image: location.state.item.image  },  
-        {    
-        commentId: 3,    
-        userId: 103,    
-        username: "jane_smith",    
-        productId: 3,    
-        rating: 5,    
-        comment: "This dress is amazing! It's so comfortable and the color is just beautiful. I got so many compliments when I wore it.",    
-        image: location.state.item.image  }]
+    const rating = [{
+        commentId: 1,
+        userId: 101,
+        username: "jane_doe",
+        productId: 1,
+        rating: 4,
+        comment: "I love this shirt! It fits well and the material is so soft. I would definitely recommend it.",
+        image: location.state.item.image
+    },
+    {
+        commentId: 2,
+        userId: 102,
+        username: "john_doe",
+        productId: 2,
+        rating: 3,
+        comment: "The shirt is okay, but the sizing is a bit off. I would suggest ordering a size up.",
+        image: location.state.item.image
+    },
+    {
+        commentId: 3,
+        userId: 103,
+        username: "jane_smith",
+        productId: 3,
+        rating: 5,
+        comment: "This dress is amazing! It's so comfortable and the color is just beautiful. I got so many compliments when I wore it.",
+        image: location.state.item.image
+    }]
 
-  
+    const addComment = () => {
+        const comment = document.getElementById('comment').value;
+        const rating = document.getElementById('rating').value;
+        const image = document.getElementById('file').value;
+        const userId = JSON.parse(localStorage.getItem('loggedInUser')).id;
+
+        const newComment = {
+            commentId: userId,
+            userId: userId,
+            username: JSON.parse(localStorage.getItem('loggedInUser')).fName,
+            productId: location.state.item.id,
+            rating: rating,
+            comment: comment,
+            image: image
+        }
+    }
+
+
     return (
         <div>
-        <Navigation />
+            <Navigation />
             <div className="Home">
                 <span>Description</span>
                 <div className="description">
@@ -51,31 +71,34 @@ function Description(props) {
                     </div>
                 </div>
             </div>
-            {/* <button className="addComment" onClick={}>Add Comment</button> */}
+            <textarea id='comment' className="commentBox" placeholder="Add a review..." required></textarea> <br />
+            <input id="id" className="rating" type="number" min="1" max="5" placeholder="Rating" required></input>/5 <br />
+            <input type="file" id="file" accept="image/*" /> 
+            <button className="addComment" onClick={addComment}>Add Comment</button>
             <div className="commentSection">
                 <h1>Comments</h1>
                 <div className="comment">
-                   { rating.map((item) => {
+                    {rating.map((item) => {
                         return (
                             <div className="commentItem">
                                 <div className="commentImage">
                                     <img src={item.image} alt="commentImage" />
-                                    </div>
-                                    <div className="commentText">
-                                        <h1>{item.username}</h1>
-                                        <p>{item.comment}</p>
-                                        <p>{item.rating}/5</p>
-                                        </div>
-                                        </div>
+                                </div>
+                                <div className="commentText">
+                                    <h1>{item.username}</h1>
+                                    <p>{item.comment}</p>
+                                    <p>{item.rating}/5</p>
+                                </div>
+                            </div>
                         )
-                   }
-                   )};
+                    }
+                    )};
                 </div>
-                </div>
-                </div>
-        
-       
-        
+            </div>
+        </div>
+
+
+
     );
 }
 
